3d9df8aff81ea9979d3e322666dc34b027edd708
kavishkamk <kavishkamadhushan727@gmail.com>
2022-04-09 02:16:04 +0530

    Initial commit

---
+++ app/src/main/java/com/kavishkamadhushan/cardock/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kavishkamadhushan.cardock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
